833517623420713cc73273cc90c32233e8f24d9e
@minhthien <>
2023-06-13 16:36:12 +0700

    Bai thuc hanh 3

---
+++ src/main/java/com/mycompany/demo_github/Demo_github.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.demo_github;
+
+/**
+ *
+ * @author MT
+ */
+public class Demo_github {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
